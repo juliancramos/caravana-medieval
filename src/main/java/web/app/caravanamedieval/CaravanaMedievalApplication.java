04dc758c109512a36dd2b3e9215a70f6f3a8c828
Julian Ramos <julianc.ramos21@gmail.com>
2025-02-21 11:44:29 -0500

    Commit inicial: Creación del proyecto

---
+++ src/main/java/web/app/caravanamedieval/CaravanaMedievalApplication.java
@@ -0,0 +1,13 @@
+package web.app.caravanamedieval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaravanaMedievalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaravanaMedievalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/app/caravanamedieval/CaravanaMedievalApplicationTests.java
@@ -0,0 +1,13 @@
+package web.app.caravanamedieval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaravanaMedievalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
