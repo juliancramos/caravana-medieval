066724b15515d778f8cbb9b47218fdfda1abc7c8
Julian Ramos <julianc.ramos21@gmail.com>
2025-02-22 10:33:24 -0500

    Configuración de la base de datos

--- src/main/java/web/app/caravanamedieval/CaravanaMedievalApplication.java
+++ src/main/java/web/app/caravanamedieval/CaravanaMedievalApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CaravanaMedievalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CaravanaMedievalApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CaravanaMedievalApplication.class, args);
+    }
 }
