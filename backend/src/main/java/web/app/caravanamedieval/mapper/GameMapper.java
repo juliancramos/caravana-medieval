fb3184c8609a56be85604f39253a073abefb6954
Julian Ramos <julianc.ramos21@gmail.com>
2025-04-03 11:25:36 -0500

    feat: Implement CRUD operations for Game

--- backend/src/main/java/web/app/caravanamedieval/mapper/GameMapper.java
+++ backend/src/main/java/web/app/caravanamedieval/mapper/GameMapper.java
@@ -2,6 +2,8 @@ package web.app.caravanamedieval.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
 import web.app.caravanamedieval.dto.GameDTO;
 import web.app.caravanamedieval.model.Caravan;
 import web.app.caravanamedieval.model.Map;
@@ -9,31 +11,25 @@ import web.app.caravanamedieval.model.Game;
 @Mapper(componentModel = "spring")
 public interface GameMapper {
 
+    GameMapper INSTANCE = Mappers.getMapper(GameMapper.class);
+
     @Mapping(source = "caravan.idCaravan", target = "caravanId")
     @Mapping(source = "map.idMap", target = "mapId")
     GameDTO toDTO(Game game);
 
-    @Mapping(source = "caravanId", target = "caravan")
-    @Mapping(source = "mapId", target = "map")
+    @Mapping(target = "idGame", ignore = true)
+    @Mapping(target = "caravan", ignore = true)
+    @Mapping(target = "map", ignore = true)
     Game toEntity(GameDTO gameDTO);
 
-    default Caravan map(Long id) {
-        if (id == null) {
-            return null;
-        }
-        Caravan caravan = new Caravan();
-        caravan.setIdCaravan(id);
-        return caravan;
-    }
+    @Mapping(target = "idGame", ignore = true)
+    @Mapping(target = "caravan", ignore = true)
+    @Mapping(target = "map", ignore = true)
+    void updateEntity(@MappingTarget Game game, GameDTO gameDTO);
 
-    default Map mapMapa(Long id) {
-        if (id == null) {
-            return null;
-        }
-        Map map = new Map();
-        map.setIdMap(id);
-        return map;
-    }
+
+
+
 }
 
 
--- backend/src/main/java/web/app/caravanamedieval/service/GameServiceImpl.java
+++ backend/src/main/java/web/app/caravanamedieval/service/GameServiceImpl.java
@@ -1,5 +1,6 @@
 package web.app.caravanamedieval.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import web.app.caravanamedieval.dto.GameDTO;
@@ -7,39 +8,76 @@ import web.app.caravanamedieval.mapper.GameMapper;
 import web.app.caravanamedieval.model.Caravan;
 import web.app.caravanamedieval.model.Map;
 import web.app.caravanamedieval.model.Game;
+import web.app.caravanamedieval.repository.CaravanRepository;
 import web.app.caravanamedieval.repository.GameRepository;
+import web.app.caravanamedieval.repository.MapRepository;
+
+import java.util.List;
 
 @Service
 public class GameServiceImpl implements GameService {
 
-    @Autowired
-    private GameMapper gameMapper;
+    private final GameRepository gameRepository;
+    private final MapRepository mapRepository;
+    private final CaravanRepository caravanRepository;
 
-    @Autowired
-    private GameRepository gameRepository;
-
-    @Autowired
-    private MapService mapService;
-
-    @Autowired
-    private CaravanService caravanService;
-
-
+    public GameServiceImpl(GameRepository gameRepository, MapRepository mapRepository, CaravanRepository caravanRepository) {
+        this.gameRepository = gameRepository;
+        this.mapRepository = mapRepository;
+        this.caravanRepository = caravanRepository;
+    }
 
     @Override
     public Game createGame(GameDTO gameDTO) {
-        Caravan caravan = caravanService.getCaravans(gameDTO.getCaravanId());
-        Map map = mapService.getMap(gameDTO.getMapId());
+        Game game = GameMapper.INSTANCE.toEntity(gameDTO);
 
-        Game game = gameMapper.toEntity(gameDTO);
+        Caravan caravan = caravanRepository.findById(gameDTO.getCaravanId())
+                .orElseThrow(() -> new EntityNotFoundException("Caravan con ID " + gameDTO.getCaravanId() + " no encontrada"));
+        Map map = mapRepository.findById(gameDTO.getMapId())
+                .orElseThrow(() -> new EntityNotFoundException("Mapa con ID " + gameDTO.getMapId() + " no encontrado"));
 
-        // Se asignan los objetos adicionales
         game.setCaravan(caravan);
         game.setMap(map);
-
         return gameRepository.save(game);
     }
 
+    @Override
+    public Game getGame(Long id) {
+        return gameRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Partida con ID " + id + " no encontrada"));
+    }
+
+    @Override
+    public List<Game> getAllGames() {
+        return gameRepository.findAll();
+    }
+
+    @Override
+    public Game updateGame(Long id, GameDTO gameDTO) {
+        Game game = gameRepository.findById(id)
+                .orElseThrow( ()-> new EntityNotFoundException("Partida con ID " + id + " no encontrada"));
+
+        GameMapper.INSTANCE.updateEntity(game, gameDTO);
+
+        //Si se cambia el id de alguna relación se cambia manualmente
+        if (gameDTO.getCaravanId() != null) {
+            Caravan caravan = caravanRepository.findById(gameDTO.getCaravanId())
+                    .orElseThrow(() -> new EntityNotFoundException("Caravan con ID " + gameDTO.getCaravanId() + " no encontrada"));
+            game.setCaravan(caravan);
+        }
+
+        if (gameDTO.getMapId() != null) {
+            Map map = mapRepository.findById(gameDTO.getMapId())
+                    .orElseThrow(() -> new EntityNotFoundException("Map con ID " + gameDTO.getMapId() + " no encontrada"));
+            game.setMap(map);
+        }
+        return gameRepository.save(game);
+    }
+
+    @Override
+    public void deleteGame(Long id) {
+        gameRepository.deleteById(id);
+    }
 
 
 }
