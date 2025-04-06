b036f35e571b43727867c741fe26b711812e9ac5
JuanR771 <juan.rozot@javeriana.edu.co>
2025-04-06 03:54:45 -0500

    fix: resolve navigation issue between pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
+import { SelectGameComponent } from './features/select-game/select-game.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'seleccionar-partida', component: SelectGameComponent }
 ];
