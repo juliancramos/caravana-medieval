9ca1aba49e166d85d289673fc8bd067898556c6b
JuanR771 <juan.rozot@javeriana.edu.co>
2025-04-07 00:01:58 -0500

    feat: add screen transition in login and select-game

--- frontend/src/app/features/login/login.component.ts
+++ frontend/src/app/features/login/login.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  fadingOut = false;
+  showBlackScreen = false;
+
   constructor(private router: Router) {}
 
   navigateToGameSelection(): void {
-    this.router.navigate(['/seleccionar-partida']);
+    this.fadingOut = true;
+    this.showBlackScreen = true;
+
+    setTimeout(() => {
+      this.router.navigate(['/seleccionar-partida']);
+    }, 900);
   }
 }
