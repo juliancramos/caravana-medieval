037681874300c8933cdae2a1130fa90f09cc4388
Julian Ramos <julianc.ramos21@gmail.com>
2025-04-10 22:49:45 -0500

    feat: link backend and frontend using CORS config

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { SelectGameComponent } from './features/select-game/select-game.componen
 import { ResumeComponent } from './features/resume/resume.component';
 import {StoreProductsComponent} from './features/store-products/store-products.component';
 import {StoreServicesComponent} from './features/store-services/store-services.component';
+import {ProductListComponent} from './features/prueba/product-list.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: 'seleccionar-partida', component: SelectGameComponent },
   { path: 'productos', component: StoreProductsComponent },
   { path: 'servicios', component: StoreServicesComponent },
-  { path: 'resume', component: ResumeComponent }
+  { path: 'resume', component: ResumeComponent },
+  { path: 'prueba', component: ProductListComponent },
 ];
