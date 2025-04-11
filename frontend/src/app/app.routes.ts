8dd2724b87f18568b730b03d2473f7590575d18c
JuanR771 <juan.rozot@javeriana.edu.co>
2025-04-11 17:05:35 -0500

    fix: rename routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,19 +1,21 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
-import { SelectGameComponent } from '@features/select-map/select-game.component';
+import { SelectMapComponent } from '@features/select-map/select-map.component';
 import { ResumeComponent } from './features/resume/resume.component';
 import { StoreProductsComponent } from './features/store-products/store-products.component';
 import { StoreServicesComponent } from './features/store-services/store-services.component';
 import { ProductListComponent } from './features/prueba/product-list.component';
 import { MapComponent } from './features/map/map.component';
+import {SelectGameComponent} from '@features/select-game/select-game.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'seleccionar-partida', component: SelectGameComponent },
+  { path: 'seleccionar-partida', component: SelectMapComponent },
   { path: 'productos', component: StoreProductsComponent },
   { path: 'servicios', component: StoreServicesComponent },
   { path: 'resume', component: ResumeComponent },
   { path: 'prueba', component: ProductListComponent },
+  { path: 'select-game', component: SelectGameComponent },
   { path: 'mapa', component: MapComponent }
 ];
