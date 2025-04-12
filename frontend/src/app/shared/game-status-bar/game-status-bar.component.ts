27a4dd8d023f84931321cabee7991ee0ce6ad3f7
JuanR771 <juan.rozot@javeriana.edu.co>
2025-04-11 23:42:18 -0500

    feat: create status-bar component

---
+++ frontend/src/app/shared/game-status-bar/game-status-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-status-bar',
+  imports: [],
+  templateUrl: './game-status-bar.component.html',
+  styleUrl: './game-status-bar.component.scss'
+})
+export class GameStatusBarComponent {
+
+}
