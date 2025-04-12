3bb1499f102207996c6e6a8924f2a851200b51f3
JuanR771 <juan.rozot@javeriana.edu.co>
2025-04-11 21:15:52 -0500

    fix: fix navigation in select-map component

--- frontend/src/app/features/select-map/select-map.component.ts
+++ frontend/src/app/features/select-map/select-map.component.ts
@@ -10,29 +10,15 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./select-map.component.scss']
 })
 export class SelectMapComponent {
-  showPopup = false;
-  fadingOut = false;
-  showBlackScreen = false;
+
+
 
   constructor(private router: Router) {}
 
-  openPopup(): void {
-    this.showPopup = true;
-  }
-
-  closePopup(): void {
-    this.showPopup = false;
-  }
 
   startGame(): void {
-    this.showBlackScreen = true;
-
-    setTimeout(() => {
-      this.router.navigate(['/resume']);
-    }, 900); // un poco más que la duración del fadeIn
+    this.router.navigate(['/resume']);
   }
 
-  loadGame(): void {
-    alert('Cargar partida');
-  }
+
 }
