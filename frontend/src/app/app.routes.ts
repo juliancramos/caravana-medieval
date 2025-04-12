87ee7520b3f1b20b64abdf53160625fafa4e3848
JuanR771 <juan.rozot@javeriana.edu.co>
2025-04-11 22:05:03 -0500

    feat: add inventory route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { ProductListComponent } from '@features/prueba/product-list.component';
 import { MapComponent } from '@features/map/map.component';
 import {SelectGameComponent} from '@features/select-game/select-game.component';
 import {SelectCaravanComponent} from '@features/select-caravan/select-caravan.component';
+import {InventoryComponent} from '@features/inventory/inventory.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -19,5 +20,6 @@ export const routes: Routes = [
   { path: 'prueba', component: ProductListComponent },
   { path: 'select-game', component: SelectGameComponent },
   { path: 'select-caravan', component: SelectCaravanComponent },
+  { path: 'inventory', component: InventoryComponent },
   { path: 'mapa', component: MapComponent }
 ];
