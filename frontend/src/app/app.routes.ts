e575819f8cbdb0ab3f1b141066488a492f6f9bbe
JuanR771 <juan.rozot@javeriana.edu.co>
2025-04-11 21:11:05 -0500

    feat: create select-caravan route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/login/login.component';
+import { LoginComponent } from '@features/login/login.component';
 import { SelectMapComponent } from '@features/select-map/select-map.component';
-import { ResumeComponent } from './features/resume/resume.component';
-import { StoreProductsComponent } from './features/store-products/store-products.component';
-import { StoreServicesComponent } from './features/store-services/store-services.component';
-import { ProductListComponent } from './features/prueba/product-list.component';
-import { MapComponent } from './features/map/map.component';
+import { ResumeComponent } from '@features/resume/resume.component';
+import { StoreProductsComponent } from '@features/store-products/store-products.component';
+import { StoreServicesComponent } from '@features/store-services/store-services.component';
+import { ProductListComponent } from '@features/prueba/product-list.component';
+import { MapComponent } from '@features/map/map.component';
 import {SelectGameComponent} from '@features/select-game/select-game.component';
 import {SelectCaravanComponent} from '@features/select-caravan/select-caravan.component';
 
