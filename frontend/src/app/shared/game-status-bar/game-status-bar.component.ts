5b5acfed9354962cd31a99b9a8016c1724998dab
Julian Ramos <julianc.ramos21@gmail.com>
2025-04-16 23:18:47 -0500

    feat: update navbar using backend

--- frontend/src/app/shared/game-status-bar/game-status-bar.component.ts
+++ frontend/src/app/shared/game-status-bar/game-status-bar.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, computed, EventEmitter, inject, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameStateService } from '@core/services/game-state.service';
+import { CurrentGameService } from '@core/services/current-game.service';
 
 @Component({
   selector: 'app-game-status-bar',
@@ -10,6 +11,26 @@ import { GameStateService } from '@core/services/game-state.service';
   styleUrls: ['./game-status-bar.component.scss']
 })
 export class GameStatusBarComponent {
+
+  currentGame = inject(CurrentGameService);
+
+  caravanName = computed(() => this.currentGame.selectedGame()?.game?.caravan?.name);
+  lifePoints = computed(() => this.currentGame.selectedGame()?.game.caravan.lifePoints);
+  availableMoney = computed(() => this.currentGame.selectedGame()?.game.caravan.availableMoney);
+  minProfit = computed(() => this.currentGame.selectedGame()?.game.minProfit);
+  cityName = computed(() => this.currentGame.selectedGame()?.game.caravan.currentCity.name);
+
+  remainingTime = computed(() => {
+    const game = this.currentGame.selectedGame()?.game;
+    if (!game) return 0;
+  
+    return game.timeLimit - game.elapsedTime;
+  });
+  
+
+
+
+
   constructor(public gameState: GameStateService) {}
 
   @Output() serviceSelected = new EventEmitter<any>();
