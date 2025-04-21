da2bdd682df1483a768e6c627800ffa4f32fc528
JuanR771 <juan.rozot@javeriana.edu.co>
2025-04-20 21:20:00 -0500

    feat: implement user register

--- frontend/src/app/features/register/register.component.ts
+++ frontend/src/app/features/register/register.component.ts
@@ -1,11 +1,72 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+import { AuthService } from '@core/services/auth.service';
+import { LoginRequest } from '@shared/models/login-request.model';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  formData = {
+    username: '',
+    password: ''
+  };
+
+  error = '';
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onSubmit(form: NgForm): void {
+    if (form.invalid) return;
+
+    const newPlayer = {
+      username: this.formData.username,
+      password: this.formData.password,
+      role: 'USER',
+      imgUrl: null
+    };
+
+    // 1. Crear jugador
+    this.http.post('/api/player/create', newPlayer).subscribe({
+      next: () => {
+        // 2. Autologin
+        const loginRequest: LoginRequest = {
+          username: this.formData.username,
+          password: this.formData.password
+        };
+
+        this.authService.login(loginRequest).subscribe({
+          next: user => {
+            this.authService.setUser(user);
+            this.playLoginTransition();
+          },
+          error: () => {
+            this.error = 'Usuario creado, pero error al iniciar sesión.';
+          }
+        });
+      },
+      error: () => {
+        this.error = 'El nombre de usuario ya está en uso.';
+      }
+    });
+  }
+
+  playLoginTransition(): void {
+    document.body.classList.add('fade-out');
+    setTimeout(() => this.router.navigate(['/game-selection']), 800);
+  }
 
 }
