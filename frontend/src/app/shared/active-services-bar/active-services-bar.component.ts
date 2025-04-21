bb7cbbd4d540aab15b3e19b001cb2cf1897d5725
JuanR771 <juan.rozot@javeriana.edu.co>
2025-04-20 21:49:27 -0500

    feat: create active-services-bar component

---
+++ frontend/src/app/shared/active-services-bar/active-services-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-active-services-bar',
+  imports: [],
+  templateUrl: './active-services-bar.component.html',
+  styleUrl: './active-services-bar.component.scss'
+})
+export class ActiveServicesBarComponent {
+
+}
