f86c9a1e22747e584b046777411ab7ffcdb0a92d
JuanR771 <juan.rozot@javeriana.edu.co>
2025-04-21 22:11:13 -0500

    feat: show current load and max capacity in game status ba

--- frontend/src/app/shared/game-status-bar/game-status-bar.component.ts
+++ frontend/src/app/shared/game-status-bar/game-status-bar.component.ts
@@ -1,37 +1,46 @@
-import { Component, computed, EventEmitter, inject, Output } from '@angular/core';
+import {Component, computed, EventEmitter, inject, OnInit, Output} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GameStateService } from '@core/services/game-state.service';
 import { CurrentGameService } from '@core/services/current-game.service';
+import {ActiveServicesBarComponent} from '@shared/active-services-bar/active-services-bar.component';
 
 @Component({
   selector: 'app-game-status-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ActiveServicesBarComponent],
   templateUrl: './game-status-bar.component.html',
   styleUrls: ['./game-status-bar.component.scss']
 })
 export class GameStatusBarComponent {
 
+
   currentGame = inject(CurrentGameService);
 
   availableMoney = this.currentGame.availableMoney;
+  activeServices = this.currentGame.activeServices;
+
 
 
   caravanName = this.currentGame.caravanName;
   lifePoints = this.currentGame.lifePoints;
-  
+
 
   minProfit = this.currentGame.minProfit;
   cityName = this.currentGame.cityName;
 
   remainingTime = this.currentGame.remainingTime;
-  
 
-  constructor(public gameState: GameStateService) {}
+  currentLoad = this.currentGame.caravanCurrentLoad;
+  maxCapacity = computed(() =>
+    this.currentGame.selectedGame()?.game.caravan.maxCapacity ?? 100
+  );
+
 
   @Output() serviceSelected = new EventEmitter<any>();
 
   showServiceInfo(service: any) {
     this.serviceSelected.emit(service);
   }
+
+
+
 }
