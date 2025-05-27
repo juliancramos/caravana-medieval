77ddeaab8662c0b9372b31fdab80aee65d2ef2bc
JuanR771 <juan.rozot@javeriana.edu.co>
2025-05-27 01:45:01 -0500

    feat(game-creation): store selected caravan and difficulty for game creation

--- frontend/src/app/core/services/game-creation.service.ts
+++ frontend/src/app/core/services/game-creation.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Caravan } from '@shared/models/caravan.model';
 import { DifficultyOption } from '@shared/models/difficulty-option.model';
 
 @Injectable({ providedIn: 'root' })
 export class GameCreationService {
+  private selectedCaravan: Caravan | null = null;
   private selectedDifficulty: DifficultyOption | null = null;
 
-  setDifficulty(difficulty: DifficultyOption) {
+ public  setCaravan(caravan: Caravan) {
+    this.selectedCaravan = caravan;
+  }
+
+  public getCaravan(): Caravan | null {
+    return this.selectedCaravan;
+  }
+
+   public setDifficulty(difficulty: DifficultyOption) {
     this.selectedDifficulty = difficulty;
   }
 
-  getDifficulty(): DifficultyOption | null {
+   public getDifficulty(): DifficultyOption | null {
     return this.selectedDifficulty;
   }
 }
